f71c9d5c235cff565497c4835686a60bac09c37d
Nick Young <young.nj21@gmail.com>
2017-03-03 00:15:16 +0000

    added mailer settings to environment file.

--- config/environment.rb
+++ config/environment.rb
@@ -5,10 +5,10 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address           => ENV['MAILGUN_SMTP_SERVER'],
-    :ser_name          => ENV['MAILGUN_SMTP_LOGIN'],
-    :password          => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain          => 'tranquil-waters-93435.herokuapp.com',
-    :authentication           => :plain,
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'tranquil-waters-93435.herokuapp.com',
+  :authentication => :plain,
 }
